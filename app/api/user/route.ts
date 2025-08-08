aa46f8c486afc462a625f4c1cd3e78c6390380a0
Nagarjun A <nagarjun.avala.official@gmail.com>
2025-08-08 19:41:38 +0530

    Fix fake API BUG

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,10 +1,10 @@
 // app/api/user/route.ts (fetching server-side)
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 import api from "@/utils/api";
 
 export async function GET() {
-  const cookieStore = await cookies();
-  const refreshToken = cookieStore.get("refreshToken"); // if needed
+  // const cookieStore = await cookies();
+  // const refreshToken = cookieStore.get("refreshToken"); // if needed
 
   try {
     const res = await api.get("/auth/profile");
